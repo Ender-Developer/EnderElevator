c4b19f80c8394452b655c1a8f11fa76083da10e7
nykoo <nykoofy@proton.me>
2026-01-15 12:27:00 -0300

    🚀 | Initial configuration of the EnderElevator project

--- src/main/java/com/github/enderdeveloper/Main.java
+++ src/main/java/com/github/enderdeveloper/Main.java
@@ -5,6 +5,8 @@ import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
 import javax.annotation.Nonnull;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main extends JavaPlugin {
 
@@ -16,5 +18,32 @@ public class Main extends JavaPlugin {
 
     @Override
     protected void setup() {
+        String currentlyYear = (new SimpleDateFormat("yyyy")).format(new Date());
+        LOGGER.atInfo().log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        LOGGER.atInfo().log("");
+        LOGGER.atInfo().log("  ______           _           _____              ");
+        LOGGER.atInfo().log(" |  ____|         | |         |  __ \\             ");
+        LOGGER.atInfo().log(" | |__   _ __   __| | ___ _ __| |  | | _____   __ ");
+        LOGGER.atInfo().log(" |  __| | '_ \\ / _` |/ _ \\ '__| |  | |/ _ \\ \\ / / ");
+        LOGGER.atInfo().log(" | |____| | | | (_| |  __/ |  | |__| |  __/\\ V /  ");
+        LOGGER.atInfo().log(" |______|_| |_|\\__,_|\\___|_|  |_____/ \\___| \\_/   ");
+        LOGGER.atInfo().log("                                                  ");
+        LOGGER.atInfo().log(" :: " + Tags.PROJECT_NAME + " :: (v" + Tags.PROJECT_VERSION + ")");
+        LOGGER.atInfo().log(" [!] Authors: https://github.com/Nxkoo (NykooX), https://github.com/JoaoAlberis (MrJoao04)");
+        LOGGER.atInfo().log(" [!] Copyright (c) 2026-" + currentlyYear);
+        LOGGER.atInfo().log(" [!] Status: Elevators Online");
+        LOGGER.atInfo().log(" [!] Github: https://github.com/Ender-Developer/EnderElevator");
+        LOGGER.atInfo().log("");
+        LOGGER.atInfo().log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    }
+
+    @Override
+    protected void shutdown() {
+        LOGGER.atInfo().log("");
+        LOGGER.atInfo().log(" [" + Tags.PROJECT_NAME + "] Systems shutting down...");
+        LOGGER.atInfo().log(" [" + Tags.PROJECT_NAME + "] Goodbye!");
+        LOGGER.atInfo().log("");
+
+        super.shutdown();
     }
 }
---
+++ src/main/java/com/github/enderdeveloper/Tags.java
@@ -0,0 +1,8 @@
+package com.github.enderdeveloper;
+
+public final class Tags {
+
+    public static final String PROJECT_NAME = "EnderElevator";
+    public static final String PROJECT_VERSION = "1.0-Alpha";
+
+}
